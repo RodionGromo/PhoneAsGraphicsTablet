2c80643b17443017142d519ef5964d5095dda473
TheGrom <49732296+RodionGromo@users.noreply.github.com>
2023-05-15 21:16:20 +0300

    massive rework, changed to udp server
    can be buggy, not extensively tested

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
-import socket, struct, win32api, win32con, pyautogui
+import socket, struct, win32api, win32con, pyautogui, time
 
-addr = ["ip","port"]
-server = socket.create_server(tuple(addr))
-
-server.listen()
-print("Server is up and running!")
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("ip","port"))
+server_socket.listen()
+print("Waiting for user!")
 width, height = pyautogui.size()
-lastPacket = time.time()
-def doStuff():
-	global lastPacket
-	clientSock, addr = server.accept()
+isLow = False
 
-	while not clientSock._closed:
-		data = clientSock.recv(10)
-		if(data == b""):
-			print("Disconnected, waiting for new client...")
+def udpServer():
+	global isLow
+	userSock, addr = server_socket.accept()
+	print(f"User connected! IP: {addr}")
+	while True:
+		try:
+			data = userSock.recv(11)
+		except ConnectionResetError:
+			print("User disconnected!")
+			break
+		if not data:
 			break
 		try:
-			decoded = struct.unpack("<bbII",data)
-		except Exception:
-			print("wrong data")
+			decoded = struct.unpack("<bbIIc",data)
+		except Exception as e:
+			print(f"wrong data: {data}")
+			print(e)
 		else:
 			x = decoded[3] / 10 ** decoded[0]
 			y = 1 - (decoded[2] / 10 ** decoded[0])
 			press = bool(decoded[1])
 			#print(f"New data: resolution: {decoded[0]}, x: {x}, y: {y}, press: {press}")
 			win32api.SetCursorPos((int(x * width), int(y * height)))
-			if(press):
+			if(press and not isLow):
+				isLow = True
 				win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-			else:
+			elif(not press and isLow):
+				isLow = False
 				win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 while True:
-	doStuff()
+	udpServer()
+	time.sleep(1)
